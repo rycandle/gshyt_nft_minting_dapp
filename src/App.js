6413a4ee78a71857af6bc9ac0c418a04684f0172
Cuality <92903262+Cuality@users.noreply.github.com>
2022-02-02 06:01:58 -0500

    overlay

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ export const StyledRoundButton = styled.button `
   padding: 10px;
   border-radius: 100%;
   border: none;
-  background-color: var(--primary);
+  background-color: var(--secondary);
   padding: 10px;
   font-weight: bold;
   font-size: 15px;
@@ -194,125 +194,104 @@ function App() {
     }, [blockchain.account]);
 
     return ( <
-        s.Screen >
-        <
-        s.Container flex = { 1 }
-        ai = { "center" }
-        style = {
-            { padding: 24, backgroundColor: "var(--primary)" } }
-        image = { CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null } >
-        <
-        StyledLogo alt = { "logo" }
-        src = { "/config/images/" }
-        /> <
-        s.SpacerSmall / >
-        <
-        ResponsiveWrapper flex = { 1 }
-        style = {
-            { padding: 24 } }
-        test >
+            s.Screen >
+            <
+            s.Container flex = { 1 }
+            ai = { "center" }
+            style = {
+                { padding: 24, backgroundColor: "var(--primary)" }
+            }
+            image = { CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null } >
+            <
+            StyledLogo alt = { "" }
+            src = { "/config/images/" }
+            /> <
+            s.SpacerSmall / >
+            <
+            ResponsiveWrapper flex = { 1 }
+            style = {
+                { padding: 24 }
+            }
+            test >
 
-        <
-        s.SpacerLarge / >
-        <
-        s.Container flex = { 2 }
-        jc = { "center" }
-        ai = { "center" }
-        style = {
-            {
-                backgroundColor: "var(--accent)",
-                padding: 24,
-                borderRadius: 24,
-                border: "4px dashed var(--secondary)",
-                boxShadow: "0px 5px 11px 2px rgba(0,0,0,0.7)",
-            }
-        } >
-        <
-        s.TextTitle style = {
-            {
-                textAlign: "center",
-                fontSize: 50,
-                fontWeight: "bold",
-                color: "var(--accent-text)",
-            }
-        } >
-        { data.totalSupply }
-        / {CONFIG.MAX_SUPPLY} <
-        /s.TextTitle> <
-        s.TextDescription style = {
-            {
-                textAlign: "center",
-                color: "var(--primary-text)",
-            }
-        } >
-        <
-        StyledLink target = { "_blank" }
-        href = { CONFIG.SCAN_LINK } > { truncate(CONFIG.CONTRACT_ADDRESS, 15) } <
-        /StyledLink> <
-        /s.TextDescription> <
-        s.SpacerSmall / > {
-            Number(data.totalSupply) >= CONFIG.MAX_SUPPLY ? ( <
-                >
-                <
-                s.TextTitle style = {
-                    { textAlign: "center", color: "var(--accent-text)" } } >
-                The sale has ended. <
-                /s.TextTitle> <
-                s.TextDescription style = {
-                    { textAlign: "center", color: "var(--accent-text)" } } >
-                You can still find { CONFIG.NFT_NAME }
-                on <
-                /s.TextDescription> <
-                s.SpacerSmall / >
-                <
-                StyledLink target = { "_blank" }
-                href = { CONFIG.MARKETPLACE_LINK } > { CONFIG.MARKETPLACE } <
-                /StyledLink> <
-                />
-            ) : ( <
-                >
-                <
-                s.TextTitle style = {
-                    { textAlign: "center", color: "var(--accent-text)" } } >
-                1 { CONFIG.SYMBOL }
-                costs { CONFIG.DISPLAY_COST } { " " } { CONFIG.NETWORK.SYMBOL }. <
-                /s.TextTitle> <
-                s.SpacerXSmall / >
-                <
-                s.TextDescription style = {
-                    { textAlign: "center", color: "var(--accent-text)" } } >
-                Excluding gas fees. <
-                /s.TextDescription> <
-                s.SpacerSmall / > {
-                    blockchain.account === "" ||
-                    blockchain.smartContract === null ? ( <
-                        s.Container ai = { "center" }
-                        jc = { "center" } >
+            <
+            s.SpacerLarge / >
+            <
+            s.Container flex = { 2 }
+            jc = { "center" }
+            ai = { "center" }
+            style = {
+                {
+                    backgroundColor: "var(--accent)",
+                    padding: 24,
+                    borderRadius: 24,
+                    border: "0px solid/dashed var(--secondary)",
+                    boxShadow: "0px 5px 11px 2px rgba(0,0,0,0.7)",
+                }
+            } >
+            <
+            s.TextTitle style = {
+                {
+                    textAlign: "center",
+                    fontSize: 50,
+                    fontWeight: "bold",
+                    color: "var(--accent-text)",
+                }
+            } > { data.totalSupply }
+            / {CONFIG.MAX_SUPPLY} < /
+            s.TextTitle > <
+            s.TextDescription style = {
+                {
+                    textAlign: "center",
+                    color: "var(--primary-text)",
+                }
+            } >
+            <
+            StyledLink target = { "_blank" }
+            href = { CONFIG.SCAN_LINK } > { truncate(CONFIG.CONTRACT_ADDRESS, 15) } <
+            /StyledLink> < /
+            s.TextDescription > <
+            s.SpacerSmall / > {
+                Number(data.totalSupply) >= CONFIG.MAX_SUPPLY ? ( <
+                    >
+                    <
+                    s.TextTitle style = {
+                        { textAlign: "center", color: "var(--accent-text)" }
+                    } >
+                    The sale has ended. <
+                    /s.TextTitle> <
+                    s.TextDescription style = {
+                        { textAlign: "center", color: "var(--accent-text)" }
+                    } >
+                    You can still find { CONFIG.NFT_NAME }
+                    on <
+                    /s.TextDescription> <
+                    s.SpacerSmall / >
+                    <
+                    StyledLink target = { "_blank" }
+                    href = { CONFIG.MARKETPLACE_LINK } > { CONFIG.MARKETPLACE } <
+                    /StyledLink> < / >
+                ) : ( <
+                        >
+                        <
+                        s.TextTitle style = {
+                            { textAlign: "center", color: "var(--accent-text)" }
+                        } >
+                        1 { CONFIG.SYMBOL }
+                        costs { CONFIG.DISPLAY_COST } { " " } { CONFIG.NETWORK.SYMBOL }. <
+                        /s.TextTitle> <
+                        s.SpacerXSmall / >
                         <
                         s.TextDescription style = {
-                            {
-                                textAlign: "center",
-                                color: "var(--accent-text)",
-                            }
+                            { textAlign: "center", color: "var(--accent-text)" }
                         } >
-                        Connect to the { CONFIG.NETWORK.NAME }
-                        network <
+                        Excluding gas fees. <
                         /s.TextDescription> <
-                        s.SpacerSmall / >
-                        <
-                        StyledButton onClick = {
-                            (e) => {
-                                e.preventDefault();
-                                dispatch(connect());
-                                getData();
-                            }
-                        } >
-                        CONNECT <
-                        /StyledButton> {
-                            blockchain.errorMsg !== "" ? ( <
-                                >
-                                <
-                                s.SpacerSmall / >
+                        s.SpacerSmall / > {
+                            blockchain.account === "" ||
+                            blockchain.smartContract === null ? ( <
+                                s.Container ai = { "center" }
+                                jc = { "center" } >
                                 <
                                 s.TextDescription style = {
                                     {
@@ -320,100 +299,122 @@ function App() {
                                         color: "var(--accent-text)",
                                     }
                                 } >
-                                { blockchain.errorMsg } <
+                                Connect to the { CONFIG.NETWORK.NAME }
+                                network <
                                 /s.TextDescription> <
-                                />
-                            ) : null
-                        } <
-                        /s.Container>
-                    ) : ( <
-                        >
-                        <
-                        s.TextDescription style = {
-                            {
-                                textAlign: "center",
-                                color: "var(--accent-text)",
+                                s.SpacerSmall / >
+                                <
+                                StyledButton onClick = {
+                                    (e) => {
+                                        e.preventDefault();
+                                        dispatch(connect());
+                                        getData();
+                                    }
+                                } >
+                                CONNECT <
+                                /StyledButton> {
+                                blockchain.errorMsg !== "" ? ( <
+                                    >
+                                    <
+                                    s.SpacerSmall / >
+                                    <
+                                    s.TextDescription style = {
+                                        {
+                                            textAlign: "center",
+                                            color: "var(--accent-text)",
+                                        }
+                                    } > { blockchain.errorMsg } <
+                                    /s.TextDescription> < / >
+                                ) : null
+                            } <
+                            /s.Container>
+                        ): ( <
+                            >
+                            <
+                            s.TextDescription style = {
+                                {
+                                    textAlign: "center",
+                                    color: "var(--accent-text)",
+                                }
+                            } > { feedback } <
+                            /s.TextDescription> <
+                            s.SpacerMedium / >
+                            <
+                            s.Container ai = { "center" }
+                            jc = { "center" }
+                            fd = { "row" } >
+                            <
+                            StyledRoundButton style = {
+                                { lineHeight: 0.4 }
                             }
-                        } >
-                        { feedback } <
-                        /s.TextDescription> <
-                        s.SpacerMedium / >
-                        <
-                        s.Container ai = { "center" }
-                        jc = { "center" }
-                        fd = { "row" } >
-                        <
-                        StyledRoundButton style = {
-                            { lineHeight: 0.4 } }
-                        disabled = { claimingNft ? 1 : 0 }
-                        onClick = {
-                            (e) => {
-                                e.preventDefault();
-                                decrementMintAmount(1);
-                            }
-                        } >
-                        -
-                        <
-                        /StyledRoundButton> <
-                        s.SpacerMedium / >
-                        <
-                        s.TextDescription style = {
-                            {
-                                textAlign: "center",
-                                color: "var(--accent-text)",
-                            }
-                        } >
-                        { mintAmount } <
-                        /s.TextDescription> <
-                        s.SpacerMedium / >
-                        <
-                        StyledRoundButton disabled = { claimingNft ? 1 : 0 }
-                        onClick = {
-                            (e) => {
-                                e.preventDefault();
-                                incrementMintAmount(1);
-                            }
-                        } >
-                        +
-                        <
-                        /StyledRoundButton> <
-                        /s.Container> <
-                        s.SpacerSmall / >
-                        <
-                        s.Container ai = { "center" }
-                        jc = { "center" }
-                        fd = { "row" } >
-                        <
-                        StyledButton disabled = { claimingNft ? 1 : 0 }
-                        onClick = {
-                            (e) => {
-                                e.preventDefault();
-                                claimNFTs();
-                                getData();
-                            }
-                        } >
-                        { claimingNft ? "BUSY" : "CLAIM" } <
-                        /StyledButton> <
-                        /s.Container> <
-                        />
-                    )
-                } <
-                />
+                            disabled = { claimingNft ? 1 : 0 }
+                            onClick = {
+                                (e) => {
+                                    e.preventDefault();
+                                    decrementMintAmount(1);
+                                }
+                            } >
+                            -
+                            <
+                            /StyledRoundButton> <
+                            s.SpacerMedium / >
+                            <
+                            s.TextDescription style = {
+                                {
+                                    textAlign: "center",
+                                    color: "var(--accent-text)",
+                                }
+                            } > { mintAmount } <
+                            /s.TextDescription> <
+                            s.SpacerMedium / >
+                            <
+                            StyledRoundButton disabled = { claimingNft ? 1 : 0 }
+                            onClick = {
+                                (e) => {
+                                    e.preventDefault();
+                                    incrementMintAmount(1);
+                                }
+                            } >
+                            +
+                            <
+                            /StyledRoundButton> < /
+                            s.Container > <
+                            s.SpacerSmall / >
+                            <
+                            s.Container ai = { "center" }
+                            jc = { "center" }
+                            fd = { "row" } >
+                            <
+                            StyledButton disabled = { claimingNft ? 1 : 0 }
+                            onClick = {
+                                (e) => {
+                                    e.preventDefault();
+                                    claimNFTs();
+                                    getData();
+                                }
+                            } > { claimingNft ? "BUSY" : "CLAIM" } <
+                            /StyledButton> < /
+                            s.Container > <
+                            />
+                        )
+                    } <
+                    />
             )
         } <
         s.SpacerMedium / >
         <
         /s.Container> <
-        s.SpacerLarge / >
+    s.SpacerLarge / >
 
         <
         /ResponsiveWrapper> <
-        s.SpacerMedium / >
+    s.SpacerMedium / >
         <
         s.Container jc = { "center" }
-        ai = { "center" }
-        style = {
-            { width: "70%" } } >
+    ai = { "center" }
+    style = {
+            { width: "70%" }
+        } >
         <
         s.TextDescription style = {
             {
@@ -425,7 +426,7 @@ function App() {
             Mainnet) and the correct address.Please note:
         Once you make the purchase, you cannot undo this action. <
         /s.TextDescription> <
-        s.SpacerSmall / >
+    s.SpacerSmall / >
         <
         s.TextDescription style = {
             {
@@ -434,13 +435,13 @@ function App() {
             }
         } >
         We have set the gas limit to { CONFIG.GAS_LIMIT }
-        for the contract to successfully mint your NFT.We recommend that you don 't lower the
-        gas limit. <
-        /s.TextDescription> <
-        /s.Container> <
-        /s.Container> <
-        /s.Screen>
-    );
+    for the contract to successfully mint your NFT.We recommend that you don 't lower the
+    gas limit. <
+        /s.TextDescription> < /
+    s.Container > <
+        /s.Container> < /
+    s.Screen >
+);
 }
 
 export default App;

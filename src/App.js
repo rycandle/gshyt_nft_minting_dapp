574961f4e3047f18fa95c0408a613d09cf372214
Cuality <92903262+Cuality@users.noreply.github.com>
2022-02-02 06:03:31 -0500

    text

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ function App() {
     const blockchain = useSelector((state) => state.blockchain);
     const data = useSelector((state) => state.data);
     const [claimingNft, setClaimingNft] = useState(false);
-    const [feedback, setFeedback] = useState(`Click claim to mint your NFT.`);
+    const [feedback, setFeedback] = useState(`Click claim to mint your FMC Card.`);
     const [mintAmount, setMintAmount] = useState(1);
     const [CONFIG, SET_CONFIG] = useState({
         CONTRACT_ADDRESS: "",
